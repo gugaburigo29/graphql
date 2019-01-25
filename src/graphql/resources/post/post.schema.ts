a0679b3ddbf1fdacf5438b4862ffcf270be1e4a3
Guga <guga.burigo29@outlook.com>
2019-01-25 18:43:23 -0200

    Cria post schema

---
+++ src/graphql/resources/post/post.schema.ts
@@ -0,0 +1,39 @@
+const postTypes = `
+    
+    # Definição do tipo post
+    type Post {
+        id: ID!
+        title: String!
+        content: String
+        photo: String
+        createdAt: String!
+        updatedAt: String!
+        author: User!
+        comments: [ Comment! ]!
+    }
+    
+    input PostInput {
+        title: String!
+        content: String
+        photo: String
+        author: Int!
+    }
+    
+`;
+
+const postQueries = `
+    posts(first: Int, offset: Int): [ Post! ]!
+    post(id: ID!): Post
+`
+
+const postMutations = `
+    createPost(input: PostInput!): Post
+    updatePost(id: ID!, input: PostInput): Post
+    deletePost(id: ID!): Boolean
+`
+
+export {
+    postTypes,
+    postQueries,
+    postMutations
+}

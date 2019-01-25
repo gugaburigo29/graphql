458840b2e2a1dac9678026db3fd97bc6db096efb
Guga <guga.burigo29@outlook.com>
2019-01-25 07:18:12 -0200

    Cria model de post

--- src/interfaces/ModelsInterface.ts
+++ src/interfaces/ModelsInterface.ts
@@ -1,5 +1,9 @@
+import {UserModel} from "../models/UserModel";
+import {PostModel} from "../models/PostModel";
+
 export interface ModelsInterface {
 
-    
+    Post: PostModel;
+    User: UserModel;
 
 }

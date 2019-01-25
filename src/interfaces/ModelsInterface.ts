44feeb92fd6efa875d7eda2eac08a89a5f090885
Guga <guga.burigo29@outlook.com>
2019-01-25 07:31:39 -0200

    Cria comment model e config do sequelize

--- src/interfaces/ModelsInterface.ts
+++ src/interfaces/ModelsInterface.ts
@@ -1,9 +1,11 @@
 import {UserModel} from "../models/UserModel";
 import {PostModel} from "../models/PostModel";
+import {CommentModel} from "../models/CommenModel";
 
 export interface ModelsInterface {
 
     Post: PostModel;
     User: UserModel;
+    Comment: CommentModel;
 
 }

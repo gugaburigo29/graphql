5473ec9a999cfb59310f0903dadc3a7aed8f1609
Guga <guga.burigo29@outlook.com>
2019-01-29 07:43:43 -0200

    Finaliza mutations de comment

--- src/graphql/resources/comment/comment.resolvers.ts
+++ src/graphql/resources/comment/comment.resolvers.ts
@@ -1,6 +1,7 @@
 import {GraphQLResolveInfo} from "graphql";
 import {DBConnection} from "../../../interfaces/DBConnectionInterface";
 import {CommentInstance} from "../../../models/CommenModel";
+import {Transaction} from "sequelize";
 
 export const commentResolvers = {
 
@@ -21,19 +22,49 @@ export const commentResolvers = {
     Query: {
 
         commentsByPost: (parent, {postId, first = 10, offset = 0}, {db}: { db: DBConnection }, info: GraphQLResolveInfo) => {
-                return db.Comment
-                    .findAll({
-                        where: {post: postId},
-                        limit: first,
-                        offset: offset
-                    })
+            return db.Comment
+                .findAll({
+                    where: {post: postId},
+                    limit: first,
+                    offset: offset
+                })
         }
 
     },
 
     Mutation: {
 
+        createComment: (parent, {input}, {db}: { db: DBConnection }, info: GraphQLResolveInfo) => {
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Comment
+                    .create(input, {transaction: t})
+            })
+        },
 
+        updateComment: (parent, {id, input}, {db}: { db: DBConnection }, info: GraphQLResolveInfo) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Comment
+                    .findById(id)
+                    .then((comment: CommentInstance) => {
+                        if (!comment) throw new Error(`Comment whit id ${id} not found`)
+                        return comment.update(input, {transaction: t})
+                    })
+            })
+        },
+
+        deleteComment: (parent, {id}, {db}: { db: DBConnection }, info: GraphQLResolveInfo) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Comment
+                    .findById(id)
+                    .then((comment: CommentInstance) => {
+                        if (!comment) throw new Error(`Comment whit id ${id} not found`)
+                        return comment.destroy({transaction: t})
+                            .then(comment => !!comment)
+                    })
+            })
+        }
 
     }
 
